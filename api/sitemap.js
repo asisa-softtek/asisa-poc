bc76407aeadc66b02a431647ca2daa72e6861428
JorgeLorenzo123 <kanyrke@gmail.com>
2026-03-12 14:14:48 +0100

    Add query index for /pokemon/*

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,15 +1,16 @@
 export default async function handler(req, res) {
-  // En una implementación real, podrías hacer fetch a PokeAPI para obtener todos los nombres
-  // Por ahora, usaremos una lista de los más populares para validar el sitemap.
-  const pokemons = [
-    'pikachu', 'charizard', 'mewtwo', 'bulbasaur', 'squirtle', 'eevee', 'lucario', 'gengar'
-  ];
+  // Fetch de todos los Pokémon
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000');
+  const data = await response.json();
+  const pokemons = data.results.map(p => p.name);
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${pokemons.map(name => `
   <url>
-    <loc>https://main--asisa-poc--asisa-softtek.aem.live/pokemon/${name}.html</loc>
+    <loc>https://asisa-poc.vercel.app/pokemon/${name}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
   </url>`).join('')}
 </urlset>`;
 
