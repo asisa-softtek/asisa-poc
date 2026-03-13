27317224b596487da0971eb2169b6e853944ad5d
Jorge Lorenzo <jorge.lorenzo@ext.softtek.com>
2026-03-13 09:48:44 +0100

    test

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,11 +1,16 @@
 export default async function handler(req, res) {
-  // En una implementación real, podrías hacer fetch a PokeAPI para obtener todos los nombres
-  // Por ahora, usaremos una lista de los más populares para validar el sitemap.
-  const pokemons = [
-    'pikachu', 'charizard', 'mewtwo', 'bulbasaur', 'squirtle', 'eevee', 'lucario', 'gengar'
-  ];
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10000');
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    if (!response.ok) {
+      throw new Error(`Error de comunicación con PokeAPI: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const pokemons = data.results.map(pokemon => pokemon.name);
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${pokemons.map(name => `
   <url>
@@ -13,6 +18,13 @@ export default async function handler(req, res) {
   </url>`).join('')}
 </urlset>`;
 
-  res.setHeader('Content-Type', 'text/xml');
-  return res.status(200).send(sitemap);
+    res.setHeader('Content-Type', 'text/xml');
+    res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
+
+    return res.status(200).send(sitemap);
+
+  } catch (error) {
+    console.error("Error generando el sitemap en el proxy:", error);
+    return res.status(500).send('<error>Error interno al generar el sitemap</error>');
+  }
 }
